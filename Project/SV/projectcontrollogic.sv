78727fda08d1df41af72b5b8044e57017fb56cb6
jcwilliamson77 <112042604+jcwilliamson77@users.noreply.github.com>
2022-12-02 11:19:48 -0600

    added files

--- Project/SV/projectcontrollogic.sv
+++ Project/SV/projectcontrollogic.sv
@@ -8,7 +8,7 @@
    
    output logic [5:0] y;
 
-   typedef enum 	logic [3:0] {S0, S1, S2, S3, S4, S5, S6, S7, S8, S9} statetype;
+   typedef enum 	logic [3:0] {S0, S1, S2} statetype;
    statetype state, nextstate;
    
    // state register
@@ -22,9 +22,6 @@
        S0: begin
 	  y <= 6'b000_000;	  
 	  if (reset) nextstate <= S0;
-    else if (Left && Right) nextstate <= S7;
-    else if (Left) nextstate <= S1;
-    else if (Right) nextstate <= S4;
 	  else   nextstate <= S0;
        end
        S1: begin
