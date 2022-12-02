4c0dc466ddaf18d95d872c7feb8a010f179bd8d4
jcwilliamson77 <112042604+jcwilliamson77@users.noreply.github.com>
2022-12-02 13:25:44 -0600

    up

--- Project/SV/projectcontrollogic.sv
+++ Project/SV/projectcontrollogic.sv
@@ -1,39 +1,13 @@
-   module FSM (clk, reset, Left, Right, y);
+module control_logic (clk, reset, seed, current_iteration, next_iteration);
+    input logic [63:0] current_iteration;
+    input logic clk;
+    input logic reset;
+    input logic [63:0] seed;
+    output logic [63:0] next_iteration;
+    logic
+    logic
 
-   input logic  clk;
-   input logic  reset;
-   input logic 	Left;
-   input logic  Right;
-   
-   
-   output logic [5:0] y;
 
-   typedef enum 	logic [3:0] {S0, S1, S2} statetype;
-   statetype state, nextstate;
-   
-   // state register
-   always_ff @(posedge clk, posedge reset)
-     if (reset) state <= S0;
-     else       state <= nextstate;
-   
-   // next state logic
-   always_comb
-     case (state)
-       S0: begin
-	  y <= 6'b000_000;	  
-	  if (reset) nextstate <= S0;
-	  else   nextstate <= S0;
-       end
-       S1: begin
-	  y <= 6'b001_000;	  	  
-	  if (~reset) nextstate <= S2;
-	  else   nextstate <= S0;
-       end
-       S2: begin
-	  y <= 6'b011_000;	  	  
-	  if (~reset) nextstate <= S3;
-	  else   nextstate <= S0;
-       end
-     endcase
+ 
    
 endmodule
