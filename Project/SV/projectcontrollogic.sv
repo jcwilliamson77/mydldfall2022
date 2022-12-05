de725d39cd104b5e52992fa6c6ef99a58498bf02
jcwilliamson77 <112042604+jcwilliamson77@users.noreply.github.com>
2022-12-05 10:55:35 -0600

    update

--- Project/SV/projectcontrollogic.sv
+++ Project/SV/projectcontrollogic.sv
@@ -4,8 +4,8 @@ module control_logic (clk, reset, seed, current_iteration, next_iteration);
     input logic reset;
     input logic [63:0] seed;
     output logic [63:0] next_iteration;
-    logic
-    logic
+    logic [63:0] y;
+    logic decision;
 
 
  
