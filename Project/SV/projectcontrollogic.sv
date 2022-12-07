1ae0876301b61953914b792a30d391ed4f990f3e
jcwilliamson77 <112042604+jcwilliamson77@users.noreply.github.com>
2022-12-07 11:38:16 -0600

    updated code

--- Project/SV/projectcontrollogic.sv
+++ Project/SV/projectcontrollogic.sv
@@ -1,17 +1,40 @@
-module FSM (clk, reset, seed, grid, grid_evolve);
-    input logic [63:0] grid;
-    input logic clk;
+module CONTROL (clk, reset, seed, switch, registerval);
+    input logic switch;
     input logic reset;
+    input logic clk;
     input logic [63:0] seed;
-    output logic [63:0] grid_evolve;
-    logic [63:0] y;
-    logic decision;
+    output logic [63:0] registerval;
+    logic [63:0] grid_evolve;
+    logic [63:0] grid;
+    logic out;
 
+    //logic [63:0] y;
+    //logic decision;
+mux mux(seed, out, grid, registerval);
+FSM fsm(clk, reset, switch, out);
 datapath generation(grid, grid_evolve);
+register register(clk, reset, grid_evolve, registerval);
 
-mux mux(seed, grid_evolve, grid, y, decision)
+endmodule
 
-typedef enum logic {S0, S1} statetype;
+module mux(FSM, seed, grid_evolve, grid);
+    input logic FSM;
+    input logic [63:0] seed, grid_evolve;
+    output logic [63:0] grid;
+
+    always_comb
+        case (FSM)
+        0: grid <= seed;
+        1: grid <= grid_evolve;
+        default: grid <= seed;
+        endcase
+endmodule
+
+module FSM(clk, reset, switch, out);
+    input logic clk, reset, switch;
+    output logic out;
+
+    typedef enum logic {S0, S1} statetype;
     statetype current_state, next_state;
 
     //state register
@@ -22,21 +45,33 @@ typedef enum logic {S0, S1} statetype;
     //next state logic and outputs
     always_comb 
       case (current_state)
-       S0: begin
-        assign grid = seed;
-    next_state = S1;
-       end
+       
+    S0: begin
+        out <= 1'b0;
+        if(switch) next_state <= S1;
+        else       next_state <= S0;
+        end
+
+    S1: begin
+        out <= 1'b0;
+        if (switch) next_state <= S1;
+        else        next_state <= S0;
+        end
+
     default: begin
-    next_state <= S0;
-    end
+        nextstate <= S0;
+        end
       endcase
 endmodule
 
-module mux(seed, decison, grid, y)
-    input logic [63:0] seed, decision, grid_evolve;
-    input logic grid;
-    output logic [63:0] y;
+module register(clk, reset, grid_evolve, registerval);
+    input logic clk, reset;
+    input logic [63:0] grid_evolve;
+    output logic [63:0] registerval;
 
-    assign y = decision?seed:grid;
+always_ff @(posedge clk, posedge reset)
+        if (reset) registerval <= S0;
+        else registerval <= grid_evolve; 
 
+  
 endmodule
